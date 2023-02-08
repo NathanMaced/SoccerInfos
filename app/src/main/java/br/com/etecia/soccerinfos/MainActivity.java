535bf666025414a0e2d606968a3f1e829f1c7dc7
NathanMaced <nathanmacedo02@gmail.com>
2023-02-07 21:24:31 -0300

    Adaptador

--- app/src/main/java/br/com/etecia/soccerinfos/MainActivity.java
+++ app/src/main/java/br/com/etecia/soccerinfos/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.soccerinfos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyAdapter adapter = new MyAdapter();
         ListaTimes = findViewById(R.id.ListaTimes);
 
-        //ListaTimes.setAdapter();
+
+        ListaTimes.setAdapter(adapter);
     }
 
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
 }
